cceac1415f9a9d4b137d9c76c08a2b49e10f968e
Chris Hampu <chrishampu@outlook.com>
2016-05-26 01:39:00 -0700

    Add corp-only check to verifySession function

--- src/core/verifySession.js
+++ src/core/verifySession.js
@@ -4,27 +4,36 @@ import { auth, eve } from '../config';
 
 async function verifySession(session) {
 
-	let user = {};
+  let user = {};
 
-	try {
-		user = jwt.verify(session.jwt, auth.jwt.secret);
+  try {
+    user = jwt.verify(session.jwt, auth.jwt.secret);
 
-		try {
+    try {
 
-			const res = await fetchCharacter(user.id);
+      const res = await fetchCharacter(user.id);
 
-			if (res.alliance !== eve.alliance_id) {
-				return { authenticated: false };
-			}
-		} catch (err) {
-			return { authenticated: false };
-		}
+      console.log("verifying");
+      console.log(res);
 
-	} catch (err) {
-		return { authenticated: false };
-	}
+      if (eve.corp_only === 'true') {
+        if (res.corporation.id !== eve.corp_id) {
+          return { authenticated: false };
+        }
+      } else {
+        if (res.alliance !== eve.alliance_id) {
+          return { authenticated: false };
+        }
+      }
+    } catch (err) {
+      return { authenticated: false };
+    }
 
-	return { authenticated: true, alliance: user.alliance, corp_id: user.corp_id, corp_name: user.corp_name, id: user.id, name: user.name };
+  } catch (err) {
+    return { authenticated: false };
+  }
+
+  return { authenticated: true, alliance: user.alliance, corp_id: user.corp_id, corp_name: user.corp_name, id: user.id, name: user.name };
 }
 
 export default verifySession;
