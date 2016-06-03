5e868c540222a03e80733c90769634417abebd2b
Chris Hampu <chrishampu@outlook.com>
2016-06-03 05:22:12 -0700

    The license was throwing linting errors...

--- src/core/fetch/fetch.server.js
+++ src/core/fetch/fetch.server.js
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2016 The Eve Quartermaster Project, Christopher Hampu, and other contributors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -43,15 +43,15 @@ function Response(body) {
   this.body = body;
 }
 
-Response.prototype.json = function() {
+Response.prototype.json = function () {
 
   return JSON.parse(this.body);
-}
+};
 
-Response.prototype.text = function() {
+Response.prototype.text = function () {
 
-  return this.body
-}
+  return this.body;
+};
 
 function Fetch(url, options) {
 
