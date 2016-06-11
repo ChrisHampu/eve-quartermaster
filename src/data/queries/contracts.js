335fd5a208ee4f3e2aa1e155e5fd28edf41a6d6f
Chris Hampu <chrishampu@outlook.com>
2016-06-11 16:30:22 -0700

    Make caching more aggressive; add timer to auto update contracts

--- src/data/queries/contracts.js
+++ src/data/queries/contracts.js
@@ -35,201 +35,220 @@ const url = `https://api.eveonline.com/corp/Contracts.xml.aspx?keyID=${eve.corp_
 let contractList = [];
 let lastFetchTask;
 let nextFetch = new Date(Date.now());
+let fulfillmentTask = null;
+let autoFetchTimer = null;
 
 // After each new contract pull from the API, this function will run async
 // and check if the contracts fulfill any requests
 async function checkFulfilledRequests(contracts) {
 
-  console.log("Beginning task to check for request fulfillment.");
-  console.time('request_fulfillment');
+	console.log("Beginning task to check for request fulfillment.");
+	console.time('request_fulfillment');
 
-  const requests = await getAllRequests();
+	const requests = await getAllRequests();
 
-  // Filter out fulfilled requests. TODO: getAllRequests() should instead filter at the SQL query level
-  requests.filter((request) => {
-    return request.status !== 'full';
-  });
+	// Filter out fulfilled requests. TODO: getAllRequests() should instead filter at the SQL query level
+	requests.filter((request) => {
+		return request.status !== 'full';
+	});
 
-  for (const contract of contracts) {
+	for (const contract of contracts) {
 
-    try {
+		try {
 
-      // Make sure its an item exchange contract
-      if (contract.type !== 0) {
-        continue;
-      }
+			// Make sure its an item exchange contract
+			if (contract.type !== 0) {
+				continue;
+			}
 
-      let items = [];
+			let items = [];
 
-      try {
-        // Pull contract items from EVE API
-        const res = await getContractItems(contract.id);
-        items = res.itemList;
+			try {
+				// Pull contract items from EVE API
+				const res = await getContractItems(contract.id);
+				items = res.itemList;
 
-      } catch (err) {
-        items = [];
-      }
+			} catch (err) {
+				items = [];
+			}
 
-      for (const request of requests) {
+			for (const request of requests) {
 
-        // Check if this request hasn't been already fulfilled
-        if (request.status === 'full') {
-          continue;
-        }
+				// Check if this request hasn't been already fulfilled
+				if (request.status === 'full') {
+					continue;
+				}
 
-        // Must be at same location
-        if (stationIDToName[contract.startStationID] !== request.station) {
-          continue;
-        }
+				// Must be at same location
+				if (stationIDToName[contract.startStationID] !== request.station) {
+					continue;
+				}
 
-        // Need to first properly group together request items
-        // There could be duplicate name entries with different amounts
-        const requestItems = [];
+				// Need to first properly group together request items
+				// There could be duplicate name entries with different amounts
+				const requestItems = [];
 
-        for (const item of request.items) {
+				for (const item of request.items) {
 
-          const idx = requestItems.findIndex((it) => {
-            return item.name === it.typeName;
-          });
+					const idx = requestItems.findIndex((it) => {
+						return item.name === it.typeName;
+					});
 
-          if (idx === -1) {
-            requestItems.push({ name: item.name, count: item.count });
-          } else {
-            requestItems[idx].count += item.count;
-          }
-        }
+					if (idx === -1) {
+						requestItems.push({ name: item.name, count: item.count });
+					} else {
+						requestItems[idx].count += item.count;
+					}
+				}
 
-        let matched = 0;
+				let matched = 0;
 
-        // Now loop through request + contract items together
-        for (const item of requestItems) {
+				// Now loop through request + contract items together
+				for (const item of requestItems) {
 
-          // Check for name match
-          const idx = items.findIndex((it) => {
-            return item.name === it.typeName;
-          });
+					// Check for name match
+					const idx = items.findIndex((it) => {
+						return item.name === it.typeName;
+					});
 
-          if (idx === -1) {
-            break;
-          }
+					if (idx === -1) {
+						break;
+					}
 
-          // Check if same number if items in request & contract
-          if (parseInt(items[idx].quantity) !== item.count) {
-            break;
-          }
+					// Check if same number if items in request & contract
+					if (parseInt(items[idx].quantity) !== item.count) {
+						break;
+					}
 
-          matched++;
-        }
+					matched++;
+				}
 
-        // Make sure the same amount of items are in the request & contract
-        if (matched === items.length && matched === request.items.length) {
+				// Make sure the same amount of items are in the request & contract
+				if (matched === items.length && matched === request.items.length) {
 
-          // TODO: Remove when no longer needed as testing output
-          console.log(`Fulfilled request "${request.title}" with ID ${request.id}`);
+					// TODO: Remove when no longer needed as testing output
+					console.log(`Fulfilled request "${request.title}" with ID ${request.id}`);
 
-          // TODO: There is currently no support for partial fulfillment or fulfilling requests for multiple contracts
-          // For tracking in this loop (making sure not fulfilled more than once)
-          request.status = 'full';
+					// TODO: There is currently no support for partial fulfillment or fulfilling requests for multiple contracts
+					// For tracking in this loop (making sure not fulfilled more than once)
+					request.status = 'full';
 
-          // Query database to update contract status
-          fulfillRequest(request, 'full');
-        }
-      }
-    } catch (err) {
-      console.log("Error while fulfilling request:");
-      console.log(err);
-    }
-  }
+					// Query database to update contract status
+					fulfillRequest(request, 'full');
+				}
+			}
+		} catch (err) {
+			console.log("Error while fulfilling request:");
+			console.log(err);
+		}
+	}
 
-  console.log("Finished request fulfillment check. Time taken:");
-  console.timeEnd('request_fulfillment');
+	fulfillmentTask = null;
+
+	console.log("Finished request fulfillment check. Time taken:");
+	console.timeEnd('request_fulfillment');
+}
+
+async function getAllContracts() {
+
+	if ((new Date(Date.now())) < nextFetch) {
+		return { contractList: contractList };
+	}
+
+	console.log("Beginning task to fetch contracts.");
+	console.time('fetch_contracts');
+
+	lastFetchTask = new fetchXML(url) // eslint-disable-line new-cap
+	.getXML()
+	.then(({ xml }) => {
+
+		contractList = [];
+
+		if (xml.eveapi.error === undefined) {
+			let contract; // eslint-disable-line vars-on-top
+
+			for (contract of xml.eveapi.result[0].rowset[0].row) {
+
+				const c = contract.$;
+
+				contractList.push({
+					id: c.contractID,
+					issuerID: c.issuerID,
+					issuerCorpID: c.issuerCorpID,
+					assigneeID: c.assigneeID,
+					acceptorID: c.acceptorID,
+					stationName: stationIDToName[c.startStationID],
+					startStationID: c.startStationID,
+					endStationID: c.endStationID,
+					type: eve.contractType[c.type],
+					status: eve.contractStatus[c.status],
+					title: c.title,
+					forCorp: c.forCorp,
+					public: c.availability === 'Public' ? 1 : 0,
+					dateIssued: c.dateIssued,
+					dateExpired: c.dateExpired,
+					dateAccepted: c.dateAccepted,
+					numDays: c.numDays,
+					dateCompleted: c.dateCompleted,
+					price: c.price,
+					reward: c.reward,
+					collateral: c.collateral,
+					buyout: c.buyout,
+					volume: c.volume
+				});
+			}
+		}
+
+		nextFetch = new Date(Date.parse(xml.eveapi.cachedUntil));
+
+		lastFetchTask = null;
+
+		if (autoFetchTimer) {
+			clearTimeout(autoFetchTimer);
+		}
+
+		// Schedule an auto fetch. This is to keep contracts up-to-date as much as possible in memory,
+		// rather than being loaded on-demand when user opens the page.
+		autoFetchTimer = setTimeout(getAllContracts, nextFetch.getTime() - Date.now() + 1000);
+
+		console.log("Finished fetching contracts. Time taken:");
+		console.timeEnd('fetch_contracts');
+		console.log(`${contractList.length} contracts loaded`);
+
+		// This function will run asynchronously as needed
+		// Tracking is used to ensure the function doesn't overlap
+		if (!fulfillmentTask) {
+			fulfillmentTask = checkFulfilledRequests(contractList);
+		}
+
+		return { contractList: contractList };
+	});
+
+	if (contractList.length) {
+		return { contractList: contractList };
+	}
+
+	return lastFetchTask;
 }
 
 const contracts = {
 
-  type: ContractListType,
-  async resolve(_, __, session) {
+	type: ContractListType,
+	async resolve(_, __, session) {
 
-    const auth = await verifySession(session);
+		const auth = await verifySession(session);
 
-    if (!auth.authenticated) {
-      console.log("Error loading contracts: User not authenticated.");
-      return { contractList: [] };
-    }
+		if (!auth.authenticated) {
+			console.log("Error loading contracts: User not authenticated.");
+			return { contractList: [] };
+		}
 
-    if (lastFetchTask) {
-      return lastFetchTask;
-    }
+		if (lastFetchTask) {
+			return lastFetchTask;
+		}
 
-    if ((new Date(Date.now())) > nextFetch) {
-
-      console.log("Beginning task to fetch contracts.");
-      console.time('fetch_contracts');
-
-      lastFetchTask = new fetchXML(url) // eslint-disable-line new-cap
-      .getXML()
-      .then(({ xml }) => {
-
-        contractList = [];
-
-        if (xml.eveapi.error === undefined) {
-          let contract; // eslint-disable-line vars-on-top
-
-          for (contract of xml.eveapi.result[0].rowset[0].row) {
-
-            const c = contract.$;
-
-            contractList.push({
-              id: c.contractID,
-              issuerID: c.issuerID,
-              issuerCorpID: c.issuerCorpID,
-              assigneeID: c.assigneeID,
-              acceptorID: c.acceptorID,
-              stationName: stationIDToName[c.startStationID],
-              startStationID: c.startStationID,
-              endStationID: c.endStationID,
-              type: eve.contractType[c.type],
-              status: eve.contractStatus[c.status],
-              title: c.title,
-              forCorp: c.forCorp,
-              public: c.availability === 'Public' ? 1 : 0,
-              dateIssued: c.dateIssued,
-              dateExpired: c.dateExpired,
-              dateAccepted: c.dateAccepted,
-              numDays: c.numDays,
-              dateCompleted: c.dateCompleted,
-              price: c.price,
-              reward: c.reward,
-              collateral: c.collateral,
-              buyout: c.buyout,
-              volume: c.volume
-            });
-          }
-        }
-
-        nextFetch = new Date(Date.parse(`${xml.eveapi.cachedUntil} UTC`));
-
-        lastFetchTask = null;
-
-        console.log("Finished fetching contracts. Time taken:");
-        console.timeEnd('fetch_contracts');
-        console.log(`${contractList.length} contracts loaded`);
-
-        // This function will run asynchronously
-        checkFulfilledRequests(contractList);
-
-        return { contractList: contractList };
-      });
-
-      if (contractList.length) {
-        return { contractList: contractList };
-      }
-
-      return lastFetchTask;
-    }
-
-    return { contractList: contractList };
-  },
+		return getAllContracts();
+	},
 };
 
 export default contracts;
